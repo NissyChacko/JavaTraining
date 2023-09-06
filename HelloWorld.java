fad279a936099c2f24904ba5500cbdbaa83e2e0a
NissyChacko <chackonissy@gmail.com>
2023-09-06 23:44:50 +0530

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+// my first programme
+
+class HelloWorld {
+	public static void main(String[] args)
+	{
+	System.out.println("Nissy Chacko");
+	System.out.println("Mundakathil House");
+        System.out.println("Payipad P.O,");
+        System.out.println("Haripad");
+        System.out.println("Allapuzha"); 
+
+
+
+	}
+
+
+}
+
+
