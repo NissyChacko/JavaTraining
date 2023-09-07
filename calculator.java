1da3e7c9ab98f749cd8318537f311274e80f6a29
NissyChacko <144264305+NissyChacko@users.noreply.github.com>
2023-09-07 22:47:34 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,12 @@
+class calculator {
+	public static void main(String[] args)
+	{
+		
+String x="Nissy Chacko";
+String y="Mundakathil House";
+String z="Payipad P.O";
+	System.out.println(x);
+	System.out.println(y);
+    System.out.println(z);
+    }
+}
