3f24036de044dc859066630f65170354136571af
NissyChacko <chackonissy@gmail.com>
2023-09-11 08:57:17 +0530

    UserInput

---
+++ UserInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class UserInput {
+  public static void main(String[] args) {
+    Scanner myScanObj = new Scanner(System.in);
+
+    System.out.println("Enter name, age and salary:");
+    // String input fir name
+    String name = myScanObj.nextLine();
+
+    // Numerical input for age
+    int age = myScanObj.nextInt();
+    // Numerical input for salary
+    double salary = myScanObj.nextDouble();
+
+    // Output input by user
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Salary: " + salary);
+  }
+}
